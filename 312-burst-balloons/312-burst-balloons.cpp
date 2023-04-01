2e5846c18871b0582210351582b7ae706a1de030
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-04-01 12:22:15 -0500

    Time: 645 ms (15.55%), Space: 383.2 MB (26.55%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -4,19 +4,20 @@ public:
         if(dp[l][r] != -1){
             return dp[l][r];
         }
+        if(l>r){
+            dp[l][r] = 0;
+            return 0;
+        }
 
         if(l==r){
             int left_count =  nums[l]*nums[l-1]*nums[r+1];
-            // cout<<l<<" "<<r<<" "<<left_count<<endl;;
             dp[l][r] = left_count;
             return left_count;
         }
-        int left_count = nums[l]*nums[l-1]*nums[r+1]+func(nums,l+1,r,dp);
-        for(int i=l+1;i<r;i++){
+        int left_count = 0;
+        for(int i=l;i<=r;i++){
             left_count = max(left_count,nums[i]*nums[l-1]*nums[r+1]+func(nums,l,i-1,dp)+func(nums,i+1,r,dp));
         }
-        left_count = max(left_count, nums[r]*nums[r+1]*nums[l-1] + func(nums,l,r-1,dp));
-        // cout<<l<<" "<<r<<" "<<left_count<<endl;
         dp[l][r] = left_count;
         return left_count;
     }
