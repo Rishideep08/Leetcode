77126853e5969a2a615d6ff8601f478221e315e1
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-04-23 14:37:30 -0500

    Time: 49 ms (91.18%), Space: 11.8 MB (85.88%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -1,26 +1,22 @@
 class Solution {
-    private : int mod = 1e9+7;
 public:
     int numberOfArrays(string s, int k) {
-        int n = s.size();
-        if(n==0){
-            return 0;
-        }
+        int n=s.size();
         vector<int> dp(n+1,0);
-        dp[n]  =1;
-        
+        dp[n] = 1;
+        int mod = 1e9+7;
         for(int i=n-1;i>=0;i--){
-            long long  sum = 0;
+            long long sum =0;
             for(int j=i;j<n;j++){
-                sum= sum*10+(s[j]-'0');  
-                if(sum <=k && sum>=1){
-                    dp[i] = (dp[j+1]+dp[i])%mod;
+                sum = sum*10+(s[j]-'0');
+                if(sum>=1 && sum<=k){
+                    dp[i] = (dp[i]+dp[j+1])%mod;
                 }else{
                     break;
                 }
             }
         }
-        return dp[0]%mod;
         
+        return dp[0];
     }
 };
