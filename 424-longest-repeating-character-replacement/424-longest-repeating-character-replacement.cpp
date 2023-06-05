1787b9379693dbbd60db018dd05966098f402589
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-06-05 14:16:23 -0500

    Time: 20 ms (32.17%), Space: 11.9 MB (5.05%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    int longestSubStr(string s,int k, char ch){
+    int helper(string s,int k,char ch){
         int start = 0;
         int end = 0;
-        int count =0;
+        int ans = 0;
         int n = s.size();
-        unordered_map<char,int> um;
         while(end<n){
-            um[s[end]]++;
-            int needToChange = end-start+1-um[ch];
-            while(needToChange >k ){
-                um[s[start]]--;
-                if(um[start] ==0 ){
-                    um.erase(start);
-                }
-                start++;
-                needToChange = end-start+1-um[ch];
+            if(s[end] != ch){
+                k--;
             }
-            count = max(count,end-start+1);
+
+            while(k<0){
+               if(s[start] != ch){
+                   k++;
+               }
+               start++;
+            }
+            
+            ans = max(ans,end-start+1);
+
             end++;
         }
-        // cout<<count<<endl;
-        return count;
+        return ans;
     }
+    
     int characterReplacement(string s, int k) {
-        int maxVal = 0;
+        int ans = 0;
         for(int i=0;i<26;i++){
-            maxVal = max(maxVal,longestSubStr(s,k,'A'+i));
+            char ch = 'A'+i;
+            ans = max(ans,helper(s,k,ch));
         }
-        return maxVal;
+        return ans;
     }
 };
