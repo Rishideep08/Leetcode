90c35229f1561dc81d2d0037745ded0e62e4807c
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-06-05 08:22:30 -0500

    Time: 16 ms (85.48%), Space: 12.6 MB (33.01%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int minMeetingRooms(vector<vector<int>>& intervals) {
-        int ans = 0;
-        int maxVal = 1e6+10;
-        vector<int> referenceArray(maxVal,0);
-        
-        for(int i=0;i<intervals.size();i++){
-            referenceArray[intervals[i][0]]+=1;
-            referenceArray[intervals[i][1]]+=-1;
+    class compareIntervals{
+        public:
+        bool operator()(const vector<int> &interval1,const vector<int> &interval2){
+            if(interval1[1]<interval2[1]){
+                return true;
+            }else if(interval1[1]==interval2[1]){
+                return interval1[0]<interval2[0];
+            }
+            return false;
         }
-        int sum = 0;
-        for(int i=0;i<maxVal;i++){
-            sum = sum+referenceArray[i];
-            ans = max(ans,sum);
+    };
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),compareIntervals());
+        int n = intervals.size();
+        multiset<int> starts;
+        int ans = 0;
+        for(int i=n-1;i>=0;i--){
+            auto itr = starts.lower_bound(intervals[i][1]);
+            int val= distance(starts.begin(),itr);
+            ans = max(ans,val+1);
+            // cout<<val<<endl;
+            
+            
+            starts.insert(intervals[i][0]);
         }
         
         return ans;
-        
     }
 };
