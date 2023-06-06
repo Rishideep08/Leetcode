c079ba04e02726f3a58e19d7fc389b9c9c0addc9
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-06-06 12:09:09 -0500

    Time: 8 ms (25.46%), Space: 9.6 MB (5.60%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        if(arr.size() <=2){
+        if(arr.size()<=2){
             return true;
         }
-        for(int i=1;i<arr.size()-1;i++){
-            if(arr[i]-arr[i-1] != arr[i+1]-arr[i]){
+        unordered_map<int,int> um;
+        int firstVal = min(arr[0],arr[1]);
+        int secondVal = max(arr[0],arr[1]);
+        um[arr[0]]++;
+        um[arr[1]]++;
+        for(int i=2;i<arr.size();i++){
+            if(arr[i]<firstVal){
+                secondVal = firstVal;
+                firstVal = arr[i];
+            }else if(arr[i]<secondVal){
+                secondVal = arr[i];
+            }
+            um[arr[i]]++;
+        }
+        int d = secondVal-firstVal;
+        int x = firstVal;
+        // cout<<firstVal<<" "<<secondVal<<endl;
+        while(um.size()>0){
+            if(um.find(x)==um.end()){
                 return false;
             }
+            um[x]--;
+            if(um[x] ==0){
+                um.erase(x);
+            }
+            x = x+d;
+            // c
         }
         return true;
+        
     }
 };
