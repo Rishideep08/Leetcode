947856d1d5bfd72d689b7dcf0767dba418eb9146
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-06-08 18:03:51 -0500

    Time: 62 ms (96.63%), Space: 16.6 MB (57.14%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    bool checkPredecessor(string word1,string word2){
-        for(int i=0;i<word1.size();i++){
-            string temp = word1.substr(0,i)+word1.substr(i+1);
-            if(word2 == temp){
-                return true;
-            }
-        }
-        return false;
-    }
-    static bool comp(string word1,string word2){
-        return word1.size()<word2.size();
-    }
+    class comp{
+        public:
+          bool operator()(const string &s1, const string &s2){
+             return s1.size()<s2.size();
+          }  
+    };
+    
     int longestStrChain(vector<string>& words) {
-        int n =  words.size();
-        if(n==0){
-            return 0;
-        }
-        sort(words.begin(),words.end(),comp);
-        unordered_map<string,int> dp;
-        dp[words[0]] = 1;
-        int maxVal = 1;
         
-        for(int i=1;i<n;i++){
-            dp[words[i]] = 1;
-            for(int j=0;j<words[i].size();j++){
-                string possiblePredecessor = (words[i]).substr(0,j)+(words[i]).substr(j+1);
-                // string temp = 
-                if(dp.find(possiblePredecessor) != dp.end()){
-                    dp[words[i]] = max(dp[words[i]],dp[possiblePredecessor]+1);
+        unordered_map<string,int> um;
+        sort(words.begin(),words.end(),comp());
+        int ans = 0;
+        int val = 0;
+        for(int i=0;i<words.size();i++){
+            string &in = words[i];
+                val = 0;
+                for(int j=0;j<in.size();j++){
+                    string temp = in.substr(0,j)+in.substr(j+1);
+                    if(um.find(temp)!=um.end()){
+                        val = max(val,um[temp]);
+                    } 
                 }
-            }
-            maxVal = max(maxVal,dp[words[i]]);
-        }    
-        return maxVal;
+                um[in] = val+1;
+                ans = max(ans,val+1);
+            
+        }
+        
+        return ans;
+        
     }
 };
