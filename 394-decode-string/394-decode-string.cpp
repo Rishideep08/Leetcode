066edbc586229962a4c48da8be73e96b18539992
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-06-12 15:02:33 -0500

    Time: 2 ms (43.52%), Space: 6.7 MB (32.29%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
     string decodeString(string str) {
-        stack<string> s;
         int n = str.size();
-        string ans;
+        stack<int> s1;
+        stack<string> s2;
+        int k=0;
+        string currString = "";
         for(int i=0;i<n;i++){
-            if(str[i] == ']'){
-                string temp = "";
-                while(!s.empty() && s.top() != "["){
-                    temp = s.top()+temp;
-                    s.pop();
+            if(isdigit(str[i])){
+                k = k*10 + (str[i]-'0');
+            }else{
+                if(str[i] == '['){
+                    s1.push(k);
+                    s2.push(currString);
+                    k=0;
+                    currString = "";
+                }else if(str[i] == ']'){
+                    int val = s1.top();
+                    s1.pop();
+                    string temp = s2.top();
+                    s2.pop();
+                    while(val){
+                        temp+=currString;
+                        val--;
+                    }
+                    currString = temp;
+                }else{
+                    currString+=str[i];
                 }
                 
-                s.pop();
-
-                // reverse(temp.begin(),temp.end());
-
-                int num = stoi(s.top());
-                s.pop();
-                string val = "";
-                while(num>0){
-                    val+=temp;
-                    num--;
-                }
-                s.push(val);
-            }else{
-                string temp = "";
-                while(i<n && (str[i]>='0' && str[i]<='9')){
-                    temp += str[i];
-                    i++;
-                }
-                if(temp == ""){
-                    s.push(string(1,str[i]));
-                }else{
-                    s.push(temp);
-                    i--;
-                }
             }
         }
-        while(!s.empty()){
-            ans = s.top()+ans;
-            s.pop();
-        }
-        return ans;
-        
+        return currString;
     }
 };
