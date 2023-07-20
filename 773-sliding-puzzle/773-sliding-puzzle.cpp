3d5312c2eed2e1e3dbaeb48ebcf8f2f8c41bca5d
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-07-20 16:09:26 -0500

    Time: 67 ms (9.82%), Space: 11.4 MB (28.13%) - LeetHub

--- 773-sliding-puzzle/773-sliding-puzzle.cpp
+++ 773-sliding-puzzle/773-sliding-puzzle.cpp
@@ -1,123 +1,75 @@
 class Solution {
 public:
-    
-    
-    bool check(vector<vector<int>>&board){
-        int count = 1;
-
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[i].size();j++){
-                // cout<<board[i][j]<<" abc ";
-                if(i==board.size()-1 && j==board[i].size()-1){
-                    continue;
-                }
-                if(board[i][j]!=count){
-                    return false;
-                }
-                count++;
-            }
-            // cout<<endl;
-            
-        }
-        // cout<<"-----\n";
-        return true;
-    }
-    
-    bool checkPossible(int i,int j,int n,int m){
-        if(i<0 || i>=n ||j<0||j>=m){
-            return false;
-        }
-        return true;
-    }
-    string convertVectortoString(vector<vector<int>> &board){
+    string convert(vector<vector<int>> &board){
         string res = "";
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[i].size();j++){
-                // cout<<board[i][j]<<" ";
-                res = res + to_string(board[i][j]);
+                res = res+to_string(board[i][j]);
             }
-            // cout<<endl;
         }
-        // cout<<"----\n";
         return res;
     }
-
-    unordered_set<string> us;
     
-    int doBfs(vector<vector<int>>& board,int n,int m){
-
+    pair<int,int> getRC(vector<vector<int>>&board){
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                if(board[i][j] == 0){
+                    return {i,j};
+                }
+            }
+        }
+        return {0,0};
+    }
+    
+    int slidingPuzzle(vector<vector<int>>& board) {
+        int n = board.size();
+        int m = board[0].size();
         
-        int dx[]={-1,0,0,1};
-        int dy[]={0,-1,1,0};
-        char dz[] = {'U','L','R','D'};
         queue<pair<vector<vector<int>>,string>> q;
         q.push({board,""});
-        string x1 = convertVectortoString(board);
-        us.insert(x1);
-        int val = INT_MAX;
-        int level=0;
+        unordered_set<string> us;
+        us.insert(convert(board));
+        
+        int dx[] = {-1,0,0,1};
+        int dy[] = {0,-1,1,0};
+        char dz[] = {'U','L','R','D'};
+        
+        int level = 0;
+        string target = "123450";
         while(!q.empty()){
-            int n1 = q.size();
-            while(n1){
+            int size = q.size();
+            while(size){
                 vector<vector<int>> temp = q.front().first;
                 string str = q.front().second;
                 q.pop();
                 
-                // for(int i=0;i<temp.size();i++){
-                //     for(int j=0;j<temp[i].size();j++){
-                //         cout<<temp[i][j]<<" ";
-                //     }
-                //     cout<<endl;
-                // }
-                // cout<<"------\n";
-                
-                if(check(temp)){
-                    cout<<str<<endl;
+                if(convert(temp) == target){
                     return level;
                 }
                 
-                int r = 0;
-                int c = 0;
-                for(int i=0;i<temp.size();i++){
-                    for(int j=0;j<temp[i].size();j++){
-                        if(temp[i][j] == 0){
-                            r = i;
-                            c = j;
-                            break;
-                        }
-                    }
-                }
+                pair<int,int> rc = getRC(temp);
+                
+                int r = rc.first;
+                int c = rc.second;
                 
                 for(int i=0;i<4;i++){
                     int r1 = r+dx[i];
                     int c1 = c+dy[i];
-
-                    if(checkPossible(r1,c1,n,m)){
+                    
+                    if(r1>=0 && r1<n && c1>=0 && c1<m){
                         swap(temp[r][c],temp[r1][c1]);
-                        string x = convertVectortoString(temp);
-                        if(us.find(x) == us.end()){
-                            q.push({temp,str + dz[i]});
-                            us.insert(x);
+                        if(us.find(convert(temp)) == us.end()){
+                            us.insert(convert(temp));
+                            q.push({temp,str+dz[i]});
                         }
                         swap(temp[r][c],temp[r1][c1]);
                     }
-                } 
-                n1--;
+                }
+                size--;
             }
-            
             level++;
         }
+        
         return -1;
-        
-        
-    }
-    
-    
-    
-    int slidingPuzzle(vector<vector<int>>& board) {
-        int n= board.size();
-        int m= board[0].size();
-        return doBfs(board,n,m);
-        
     }
 };
