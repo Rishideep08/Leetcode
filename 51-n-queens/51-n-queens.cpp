473881d7aa96463e2c67caaf8b72dddec70a4a01
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-07-26 14:08:03 -0500

    Time: 6 ms (72.27%), Space: 7.3 MB (53.83%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,98 @@
+class Solution {
+public:
+    
+    
+    vector<vector<string>> res;
+    
+    bool isSafe(vector<string> &pattern,int r,int c){
+        int n = pattern.size();
+        for(int i=0;i<n;i++){
+            if(pattern[i][c] == 'Q' ){
+                return false;
+            }
+        }
+        int r1 = r;
+        int c1 = c;
+        while(r1>=0 && c1>=0){
+            if(pattern[r1][c1] =='Q'){
+                return false;
+            }
+            r1--;
+            c1--;
+        }
+        
+        
+        r1 = r;
+        c1 = c;
+        while(r1<n && c1<n){
+            if(pattern[r1][c1] =='Q'){
+                return false;
+            }
+            r1++;
+            c1++;
+        }
+        
+        r1 = r;
+        c1 = c;
+        while(r1<n && c1>=0){
+            if(pattern[r1][c1] =='Q'){
+                return false;
+            }
+            r1++;
+            c1--;
+        }
+        
+        r1 = r;
+        c1 = c;
+        while(r1>=0 && c1<n){
+            if(pattern[r1][c1] =='Q'){
+                return false;
+            }
+            r1--;
+            c1++;
+        }
+        
+        
+        // for(int i=0;i<n;i++){
+        //     if(pattern[r][i] == 'Q' ){
+        //         return false;
+        //     }
+        // }
+        
+        return true;
+    }
+    
+    
+    void func(int index,vector<string> &pattern,int n){
+        if(n==index){
+            res.push_back(pattern);
+            return;
+        }
+        
+        for(int j=0;j<n;j++){
+            if(isSafe(pattern,index,j)){
+                pattern[index][j] = 'Q';
+                func(index+1,pattern,n);
+                pattern[index][j] = '.';
+            }
+        }
+        
+        return;
+        
+    }
+    
+    
+    vector<vector<string>> solveNQueens(int n) {
+        string str = "";
+        for(int i=0;i<n;i++){
+            str+='.';
+        }
+        // cout<<str<<endl;
+        vector<string> pattern(n,str);
+        // for(int i=0;i<n;i++){
+        //     cout<<pattern[i]<<endl;
+        // }
+        func(0,pattern,n);
+        return res;
+    }
+};
