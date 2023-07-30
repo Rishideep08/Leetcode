28783da15dd5c12aa58ee6c0cbb5b2ad600c8c37
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-07-30 12:55:25 -0500

    Time: 9 ms (79.57%), Space: 7.1 MB (99.05%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cpp
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -1,36 +1,39 @@
+#include <semaphore.h>
 class Foo {
 private:
-    atomic<int> x1;
-    atomic<int> x2;
+    sem_t x1;
+    sem_t x2;
 public:
     Foo() {
-        x1 = 0;
-        x2 = 0;
+        sem_init(&x1,0,0);
+        sem_init(&x2,0,0);
     }
 
     void first(function<void()> printFirst) {
         
         // printFirst() outputs "first". Do not change or remove this line.
         printFirst();
-        x1++;
+        // x1++;
+        sem_post(&x1);
         // cout<<x1<<endl;
     }
 
     void second(function<void()> printSecond) {
         
         // printSecond() outputs "second". Do not change or remove this line.
-        while(x1==0){
-        }
+        sem_wait(&x1);
         printSecond();
-        x2++;
+        // x2++;
+        sem_post(&x2);
     }
 
     void third(function<void()> printThird) {
         
         // printThird() outputs "third". Do not change or remove this line.
-        while(x2==0){
+//         while(x2==0){
             
-        }
+//         }
+        sem_wait(&x2);
         printThird();
     }
 };
