3d54d8b21c0140eb3ee5d583a8b5b545edfd3d55
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-08-26 20:53:02 -0500

    Time: 39 ms (65.85%), Space: 14.9 MB (19.51%) - LeetHub

--- 1737-change-minimum-characters-to-satisfy-one-of-three-conditions/1737-change-minimum-characters-to-satisfy-one-of-three-conditions.cpp
+++ 1737-change-minimum-characters-to-satisfy-one-of-three-conditions/1737-change-minimum-characters-to-satisfy-one-of-three-conditions.cpp
@@ -1,60 +1,32 @@
 class Solution {
 public:
-    int helper(string a,string b){
-        int i=0,j=0;
-        vector<int> arr(26,0),arr1(26,0);
-        for(int i=0;i<a.size();i++){
-            arr[a[i]-'a']++;
-        }
-        
-        for(int i=0;i<b.size();i++){
-            arr1[b[i]-'a']++;
-        }
-        
-        for(int i=1;i<26;i++){
-            arr[i]+=arr[i-1];
-            arr1[i]+=arr1[i-1];
-        }
-        int ans = INT_MAX;
-        for(int i=1;i<26;i++){
-            int val = a.size();
-            if(i!=0){
-                val = val-arr[i-1];
-            }
-            int val1 = 0;
-            if(i!=0){
-                val1 = val1+arr1[i-1];
-            }
-            ans = min(ans,val+val1);
-        }
-        // cout<<ans<<endl;
-        return ans;
-    }
-    
-    int helper2(string a,string b){
-        unordered_map<int,int> um;
-        for(int i=0;i<a.size();i++){
-            um[a[i]]++;
-        }
-        for(int i=0;i<b.size();i++){
-            um[b[i]]++;
-        }
-        
-        int val = a.size()+b.size();
-        int ans = INT_MAX;
-        for(int i=0;i<26;i++){
-            ans = min(ans,val-um['a'+i]);
-        }
-        // cout<<ans;
-        
-        return ans;
-        
-    }    
-    
-    
     int minCharacters(string a, string b) {
+        vector<vector<int>> lookup(2,vector<int>(26,0));
         
-        return min(helper(a,b),min(helper(b,a),helper2(a,b)));
+        for(int i=0;i<a.size();i++){
+            lookup[0][a[i]-'a']++;
+        }
         
+        
+        for(int i=0;i<b.size();i++){
+            lookup[1][b[i]-'a']++;
+        }
+        int n = a.size();
+        int m = b.size();
+        int ans = n+m-lookup[0][0]-lookup[1][0];
+        for(int i=1;i<26;i++){
+            ans = min(ans,n+m-lookup[0][i]-lookup[1][i]);
+            ans = min(ans,min(lookup[0][i-1]+m-lookup[1][i-1],n-lookup[0][i-1]+lookup[1][i-1]));
+            lookup[0][i]+=lookup[0][i-1];
+            lookup[1][i]+=lookup[1][i-1];
+        }
+        
+        
+        
+    
+        
+        
+        
+        return ans;
     }
 };
