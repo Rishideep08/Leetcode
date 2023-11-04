40f5f4662550102fc388b47b4ee0a307f1b7c646
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-11-04 04:29:39 -0500

    Time: 2587 ms (5.21%), Space: 393.7 MB (5.21%) - LeetHub

--- 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int n, time, allOnes;
-    int ans;
-    int dp[1<<15][16];
-    int minSessions(vector<int>& a, int t) {
-        time = t;
-        n = a.size();
-        allOnes = (1<<n)-1;
-        memset(dp, -1, sizeof dp);
-        ans = help(a, 0, 0);
-        return ans;
-    }
-    
-    int help(vector<int> &a, int mask, int currTime){
-        if(mask == allOnes) return 0;
-        if(dp[mask][currTime] != -1) return dp[mask][currTime];
-        int ans = INT_MAX;
-        for(int i = 0 ; i < n ; i++){
-            if( (mask & (1<<i)) == 0){
-                ans = min(ans,1 + help(a, mask | (1<<i), time-a[i]));
-                if(currTime >= a[i] ){
-                ans = min(ans,help(a, mask | (1<<i), currTime - a[i]));
+    vector<vector<int>> dp;
+    int func(vector<int>& tasks,int sessionTime,int remainTime,int index,int mask){
+        if(index == tasks.size()){
+            return 0;
+        }
+        if( dp[mask][remainTime] != -1){
+            return dp[mask][remainTime];
+        }
+        
+        int ans = tasks.size();
+        for(int i=0;i<tasks.size();i++){
+            if((mask & (1<<i)) == 0){
+                ans = min(ans,func(tasks,sessionTime,sessionTime-tasks[i],index+1,mask | (1<<i))+1);
+                if(remainTime>=tasks[i]){
+                    ans = min(ans,func(tasks,sessionTime,remainTime-tasks[i],index+1,mask | (1<<i)));
                 }
             }
         }
-        return dp[mask][currTime] = ans;
+        
+        dp[mask][remainTime] = ans;
+        return ans;
+        
+    }
+    
+    int minSessions(vector<int>& tasks, int sessionTime) {
+        int n = tasks.size();
+        dp = vector<vector<int>>(1e5,vector<int>(20,-1));
+        return func(tasks,sessionTime,0,0,0); 
     }
 };
