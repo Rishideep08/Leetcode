cae70819cd1dc47a8981a6b9d3ba933c0e87abd9
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-11-06 15:01:35 -0600

    Time: 403 ms (5.06%), Space: 188 MB (5.05%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int func(string s, int l, int r){
+    int func(string s,int l,int r){
         if(l>r){
             return 0;
         }
         if(l==r){
             return 1;
         }
-        
-        if(dp[l][r] != -1){
+        if(dp[l][r] !=-1){
             return dp[l][r];
         }
+        
         int ans = 1+func(s,l+1,r);
+        
         for(int i=l+1;i<=r;i++){
             if(s[i] == s[l]){
                 ans = min(ans,func(s,l,i-1)+func(s,i+1,r));
@@ -23,8 +24,7 @@ public:
     }
     
     int strangePrinter(string s) {
-        int n = s.size();
-        dp = vector<vector<int>>(n+1,vector<int>(n+1,-1));
-        return func(s,0,n-1);
+        dp = vector<vector<int>>(s.size(),vector<int>(s.size(),-1));
+        return func(s,0,s.size()-1);
     }
 };
