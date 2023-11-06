4e2af164748ff47144514beab1f13bac7705b424
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-11-06 14:57:32 -0600

    Time: 5 ms (31.20%), Space: 8.3 MB (95.22%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,60 +1,47 @@
 class Solution {
 public:
-    unordered_map<int,vector<string>>um;
+    vector<string> ans;
+    unordered_map<int,vector<string>> um;
     
     vector<string> func(int n ){
         if(n==0){
             vector<string> temp;
-            return temp;
-        }
-        if(n==1){
-            vector<string> temp;
-            string ans = "()";
-            temp.push_back(ans);
+            temp.push_back("");
             return temp;
         }
         
+        if(n==1){
+           vector<string> temp;
+            temp.push_back("()"); 
+            return temp;
+        }
         if(um.find(n) != um.end()){
             return um[n];
         }
+        
         vector<string> ans;
-        for(int i=0;i<n;i++){
-            vector<string> temp1 = func(i);
-            vector<string> temp2 = func(n-i-1);
-            for(int j=0;j<temp1.size();j++){
-                string ans1 = "(";
-                ans1+=temp1[j];
-                ans1+=')';
-                for(int k=0;k<temp2.size();k++){
-                    string ans2 = ans1+temp2[k];
-                    ans.push_back(ans2);
+        for(int i=0;i<=n-1;i++){
+            vector<string> inner = func(i);
+            vector<string> outer = func(n-1-i);
+            string res ="(";
+            for(int j=0;j<inner.size();j++){
+                string res1= res+inner[j];
+                res1 = res1+')';
+                for(int k=0;k<outer.size();k++){
+                    string res2 = res1+outer[k];
+                    ans.push_back(res2);
                 }
-                if(temp2.size() == 0){
-                    ans.push_back(ans1);
-                }
+                
             }
         }
-         vector<string> temp2 = func(n-1);
-        string ans1 = "(";
-        ans1+=')';
-        for(int k=0;k<temp2.size();k++){
-            string ans2 = ans1+temp2[k];
-            ans.push_back(ans2);
-        }
-        
         um[n] = ans;
-        
         return ans;
-        
-        
-        
     }
     
-    vector<string> generateParenthesis(int n) {
-        return func(n);
-        
-        
     
+    
+    vector<string> generateParenthesis(int n) {
         
+        return func(n);
     }
 };
