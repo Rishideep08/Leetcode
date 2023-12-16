c0932208ca721fa5de91d2ca99a90b311cb3b3fa
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-12-15 22:33:46 -0600

    Time: 6 ms (13.93%), Space: 10.8 MB (13.57%) - LeetHub

---
+++ 0666-path-sum-iv/0666-path-sum-iv.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int pathSum(vector<int>& nums) {
+        unordered_map<int,unordered_map<int,int>> um;
+        for(int num : nums){
+            if(um.find(num/100) == um.end()){
+                unordered_map<int,int> um1;
+                um[num/100] = um1;
+            }
+            um[num/100][(num/10)%10] = num%10;
+        }
+        unordered_map<int,int> um1;
+        um[5] = um1;
+        int ans = 0;
+        for(int i=1;i<5;i++){
+            for(auto it = um[i].begin();it != um[i].end();it++){
+                int key = it->first;
+                int val = it->second;
+                int flag = 0;
+                if(um[i+1].find(2*key) != um[i+1].end()){
+                    um[i+1][2*key] += um[i][key];
+                    flag = 1;
+                } 
+                if(um[i+1].find(2*key-1) != um[i+1].end()){
+                    um[i+1][2*key-1] += um[i][key];
+                    flag = 1;
+                } 
+                if(flag == 0){
+                    ans = ans + um[i][key];
+                }
+            }
+        }
+        return ans;
+    }
+};
