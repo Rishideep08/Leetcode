18ecba997efe76aa499c940a640796772c9bb073
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2023-12-22 00:43:02 -0600

    Time: 112 ms (46.12%), Space: 82.9 MB (43.67%) - LeetHub

--- 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.cpp
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int minimumOperations(vector<int>& nums1) {
+    int minimumOperations(vector<int>& nums) {
         int i=0;
-        int j = nums1.size()-1;
-        vector<long long int> nums(nums1.size(),0);
-        for(int i=0;i<nums1.size();i++){
-            nums[i] = nums1[i];
-        }
+        int j = nums.size()-1;
+        // vector<long long int> nums(nums1.size(),0);
+        // for(int i=0;i<nums1.size();i++){
+        //     nums[i] = nums1[i];
+        // }
         int count = 0;
         while(i<=j){
             if(nums[i] == nums[j]){
                 i++;
                 j--;
             }else if(nums[i]>nums[j]){
+                nums[j]-=nums[i];
+                nums[i]-=nums[i];
                 nums[j-1] += nums[j];
                 j--;
                 count++;
             }else{
+                nums[i]-=nums[j];
+                nums[j]-=nums[j];
                 nums[i+1] +=nums[i];
                 i++;
                 count++;
