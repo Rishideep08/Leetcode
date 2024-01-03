a3afbd3c6fc65a23b1001b1b8c579bd17cf53bd4
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-03 11:23:38 -0600

    Time: 15 ms (42.65%), Space: 9.4 MB (46.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -19,13 +19,34 @@ public:
                 }
             }
         }
+        string res="";
+        int i = word1.size();
+        int j = word2.size();
         
-        for(int i=0;i<=word1.size();i++){
-            for(int j=0;j<=word2.size();j++){
-                cout<<dp[i][j]<<" ";
+        while(i>0 && j>0){
+            if(word1[i-1] == word2[j-1]){
+                res+=word2[j-1];
+                i--;
+                j--;
+            }else{
+                if(dp[i-1][j] < dp[i-1][j-1] && dp[i-1][j] < dp[i][j-1] ){
+                    res += 'D';
+                    res += word1[i-1];
+                    i--;
+                }else if(dp[i][j-1] < dp[i-1][j-1] && dp[i][j-1] < dp[i-1][j]){
+                    res += 'A';
+                    res += word2[j-1];
+                    j--;
+                }else{
+                    res += 'R';
+                    res += word2[j-1];
+                    j--;
+                    i--;
+                }
             }
-            cout<<endl;
         }
+        
+        cout<<res<<endl;
         return dp[word1.size()][word2.size()];
     }
 };
