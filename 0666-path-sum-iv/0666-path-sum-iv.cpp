ab738ce166b8e208739045dfbcea827c79e4ebb3
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-05 15:04:27 -0600

    Time: 6 ms (16.28%), Space: 10.6 MB (36.54%) - LeetHub

--- 0666-path-sum-iv/0666-path-sum-iv.cpp
+++ 0666-path-sum-iv/0666-path-sum-iv.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
     int pathSum(vector<int>& nums) {
-        unordered_map<int,unordered_map<int,int>> um;
-        for(int num : nums){
-            if(um.find(num/100) == um.end()){
-                unordered_map<int,int> um1;
-                um[num/100] = um1;
-            }
-            um[num/100][(num/10)%10] = num%10;
+        if(nums.size() == 0){
+            return 0;
         }
-        unordered_map<int,int> um1;
-        um[5] = um1;
+        vector<vector<int>> adjList(5);
         int ans = 0;
-        for(int i=1;i<5;i++){
-            for(auto it = um[i].begin();it != um[i].end();it++){
-                int key = it->first;
-                int val = it->second;
-                int flag = 0;
-                if(um[i+1].find(2*key) != um[i+1].end()){
-                    um[i+1][2*key] += val;
-                    flag = 1;
-                } 
-                if(um[i+1].find(2*key-1) != um[i+1].end()){
-                    um[i+1][2*key-1] += val;
-                    flag = 1;
-                } 
-                if(flag == 0){
-                    ans = ans +val;
-                }
-            }
+        for(int i=0;i<5;i++){
+            int size = int(pow(2,i));
+            vector<int> temp(size,-1);
+            adjList[i] = temp;
         }
+        
+        for(int num : nums){
+            adjList[(num/100)-1][((num/10)%10)-1] = num%10;
+        }
+        
+        // for(int i=0;i<4;i++){
+        //     for(int j=0;j<adjList[i].size();j++){
+        //         cout<<adjList[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        for(int i=0;i<4;i++){
+           for(int j=0;j<adjList[i].size();j++){
+               if(adjList[i][j] == -1){
+                   continue;
+               }
+               int flag = 0;
+               if(adjList[i+1][2*j] != -1){
+                   adjList[i+1][2*j]+=adjList[i][j];
+                   flag = 1;
+               }
+               if(adjList[i+1][2*j+1] != -1){
+                   adjList[i+1][2*j+1]+=adjList[i][j];
+                   flag = 1;
+               }
+               
+               if(flag == 0){
+                   ans = ans + adjList[i][j];
+               }
+           }
+        }
+    
+        
+        
         return ans;
     }
 };
