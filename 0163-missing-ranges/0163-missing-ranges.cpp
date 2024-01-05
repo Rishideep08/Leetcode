fe2c445a51f5a63f3e9e6814052f4fa5b154bad9
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-05 17:30:19 -0600

    Time: 0 ms (100.00%), Space: 7.5 MB (13.24%) - LeetHub

---
+++ 0163-missing-ranges/0163-missing-ranges.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    vector<int> func(int l,int r){
+        vector<int> temp;
+        temp.push_back(l);
+        temp.push_back(r);
+        return temp;
+    }
+    
+    
+    vector<vector<int>> findMissingRanges(vector<int>& nums, int l, int r) {
+        vector<vector<int>> res;
+        int flag = 0;
+        int n = nums.size();
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]>=l && nums[i]<=r){
+                flag = 1;
+                if(i==0 || nums[i-1]<l){
+                    if(l<=nums[i]-1)
+                        res.push_back(func(l,nums[i]-1));
+                }
+                
+                if(i+1 == n || (nums[i+1]>r)){
+                    if(nums[i]+1<=r)
+                        res.push_back(func(nums[i]+1,r));
+                }else{
+                    if(nums[i]+1<=nums[i+1]-1)
+                        res.push_back(func(nums[i]+1,nums[i+1]-1));
+                }    
+            }
+
+        }
+        
+        if(flag == 0){
+                res.push_back(func(l,r));
+          }
+        return res;
+    }
+};
