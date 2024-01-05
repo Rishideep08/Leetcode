12470ac5d7eb8cc9c7f0c216697744e949e970e8
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-05 14:29:15 -0600

    Time: 0 ms (100.00%), Space: 6.4 MB (94.83%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.cpp
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int scoreOfParentheses(string str) {
-        int n= str.size();
-        stack<int> s;
         int curr = 0;
-        for(int i=0;i<n;i++){
-            if(str[i] == '('){
-                s.push(curr);
-                curr = 0;
-            }else{
-                if(curr == 0){
-                    int x = 0;
-                    if(!s.empty()){
-                        x = s.top();
-                        s.pop();
-                    }
-                    curr = x+1;
+        stack<pair<int,int>> s;
+        for(int i=0;i<str.size();i++){
+            if(str[i] == ')'){
+                int startIndex = s.top().first;
+                int prevCurr = s.top().second;
+                s.pop();
+                
+                if(startIndex == i-1){
+                    curr = 1;
                 }else{
-                    int x = 0;
-                    if(!s.empty()){
-                        x = s.top();
-                        s.pop();
-                    }
-                    curr = x+2*curr;
+                    curr = 2*curr;
                 }
+                
+                curr = curr + prevCurr;
+            }else{
+                s.push({i,curr});
+                curr = 0;
             }
         }
         return curr;
-        
     }
 };
