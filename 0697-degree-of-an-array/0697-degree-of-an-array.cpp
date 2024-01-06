aa7cdb8bc19340ffdc70008a8ef5119c69a1896b
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-06 12:54:12 -0600

    Time: 28 ms (73.84%), Space: 25.8 MB (64.97%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -2,30 +2,36 @@ class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
         unordered_map<int,int> um;
-        int n = nums.size();
-        for(int num: nums){
+        for(int num : nums){
             um[num]++;
         }
-        int max_freq = 0;
-        for(auto it = um.begin();it!=um.end();it++){
-            max_freq = max(max_freq,it->second);
+        int maxFreq = -1;
+        int maxVal = -1;
+        for(auto itr = um.begin();itr!=um.end();itr++){
+            if(maxFreq<itr->second){
+                maxFreq = itr->second;
+            }
         }
-        unordered_map<int,pair<int,int>> maxFreqNums;
+        
+        
+        int l = 0,r=nums.size()-1;
+        int ans = r-l+1;
+        unordered_map<int,int> left,right;
         for(int i=0;i<nums.size();i++){
-            if(um[nums[i]] == max_freq){
-                if(maxFreqNums.find(nums[i]) == maxFreqNums.end()){
-                    maxFreqNums[nums[i]] = {i,i};
-                }else{
-                     maxFreqNums[nums[i]].second = i;
+            if(um[nums[i]]==maxFreq){
+                if(left.find(nums[i]) == left.end()){
+                    left[nums[i]] = i;
                 }
             }
         }
-        int ans = INT_MAX;
-        for(auto it = maxFreqNums.begin();it!=maxFreqNums.end();it++){
-            pair<int,int> temp = it->second;
-            // cout<<it->first<<" "<<temp.first<<" "<<temp.second<<endl;
-            ans = min(ans,temp.second - temp.first+1);
+        for(int i=nums.size()-1;i>=0;i--){
+            if(um[nums[i]]==maxFreq){
+                ans = min(ans,i-left[nums[i]]+1);
+                um[nums[i]]--;
+            }
         }
+        
         return ans;
+        
     }
 };
