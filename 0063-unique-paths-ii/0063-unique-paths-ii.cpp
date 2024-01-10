d09386f8908af8dd5710a154011295bf73398981
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-09 19:37:49 -0600

    Time: 2 ms (63.79%), Space: 8.1 MB (47.93%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -13,13 +13,9 @@ public:
         
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         
-        
+        dp[0][1] = 1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(i==1 && j==1){
-                    dp[i][j] = 1;
-                    continue;
-                }
                 if(grid[i-1][j-1] == 1){
                     dp[i][j] = 0;
                 }else{
