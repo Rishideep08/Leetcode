4c25a6e23179c4734e01d7a75817923ad654ffb2
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-11 20:08:20 -0600

    Time: 0 ms (100.00%), Space: 11.9 MB (12.02%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long int maxVal = 0;
+        int count = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>maxVal){
+                if(nums[i]-maxVal>1){
+                    while(nums[i]-maxVal>1){
+                        if(maxVal>=n){
+                            return count;
+                        }
+                        maxVal = maxVal*2+1;
+                        count++;
+                    }
+                    maxVal = maxVal+nums[i];
+                }else{
+                    maxVal =maxVal+nums[i];
+                }
+            }else{
+                 maxVal = maxVal+nums[i];
+            }
+           if(maxVal>=n){
+               return count;
+           }
+        }
+        
+        while(n>maxVal){
+            maxVal = maxVal*2+1;
+            count++;
+        }
+        
+        return count;
+    }
+};
