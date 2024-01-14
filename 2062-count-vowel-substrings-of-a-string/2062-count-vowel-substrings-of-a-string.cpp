88f1993017f3a5c167a6cf406cada60eebd2f75c
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-14 15:38:02 -0600

    Time: 0 ms (100.00%), Space: 6.5 MB (92.60%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.cpp
@@ -2,21 +2,44 @@ class Solution {
 public:
     int countVowelSubstrings(string word) {
         unordered_map<char,int> um;
-        int ans = 0;
+        int curr = 0;
+        int start = 0;
+        int end = 0;
+        int possibleStarts =0;
+        int res = 0;
+        int prev =0 ;
         for(int i=0;i<word.size();i++){
-            um.clear();
-            for(int j=i;j<word.size();j++){
-                char ch = word[j];
-                if(ch == 'a' || ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                    um[ch]++;
-                }else{
-                    break;
-                }
-                if(um.size()==5){
-                    ans++;
-                }
+            char ch = word[i];
+            if(ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                um[ch]++;
+                // int flag = 0;
+                while(um.size() == 5){
+                    // cout<<i<<" "<<start<<endl;
+                    // flag = 1;
+                    possibleStarts++;
+                    um[word[start]]--;
+                    if(um[word[start]]==0){
+                        um.erase(word[start]);
+                    }
+                    start++;
+                }  
+                curr = prev+ possibleStarts;
+                // if(flag == 1){
+                //     curr--;
+                // }
+                res = res+curr;
+                // if(res == 1){
+                //     cout<<start<<" "<<i<<endl;
+                // }
+                prev = curr;
+                possibleStarts = 0;
+            }else{
+                start = i+1;
+                possibleStarts = 0;
+                prev = 0;
+                um.clear();
             }
         }
-        return ans;
+        return res;
     }
 };
