6194ea763f2844d1da03fe46d106302575d11fce
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-01-17 17:37:43 -0600

    Time: 82 ms (99.02%), Space: 68 MB (61.11%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,45 +1,50 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n  = nums.size();
         int start = 0;
-        int end = 0;
-        int possibleEnds =0;
-        int ans=0;
-        int count =0;
+        int end =0;
+        int ans =0;
+        int possibleEnds= 0;
+        int count = 0;
+        int n= nums.size();
         while(end<n){
             if(nums[end]%2 == 1){
                 count++;
             }
             if(count == k){
+                // cout<<"Hi "<<start<<" "<<end<<endl;
                 possibleEnds++;
                 ans++;
-            }else if(count>k){
+            }else if(count > k){
+                // cout<<"addfdf"<<endl;
+                if(nums[start]%2 == 1){
+                        count--;
+                }
+                start++;
                 while(count>k){
-                    if(nums[start]%2==0){
-                        ans= ans+possibleEnds;
-                    }else{
+                    ans = ans+possibleEnds;
+                    if(nums[start]%2 == 1){
                         count--;
                     }
                     start++;
-                }
-                cout<<end<<" "<<start<<" "<<ans<<endl;
+                } 
                 possibleEnds = 1;
                 ans++;
             }
             end++;
         }
+        
         if(nums[start]%2 == 1){
-            return ans;
+                count--;
         }
         start++;
-        while(start<n){
+        while(start<n && count==k){
             ans = ans+possibleEnds;
             if(nums[start]%2 == 1){
-                break;
+                count--;
             }
             start++;
-        }
+        } 
         return ans;
     }
 };
