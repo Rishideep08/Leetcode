d2e8ad0ff554b67051947598d612d183551c0b79
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-02-07 16:10:22 -0600

    Time: 0 ms (100.00%), Space: 8.2 MB (17.42%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.cpp
+++ 0163-missing-ranges/0163-missing-ranges.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
-    
-    vector<int> func(int l,int r){
-        vector<int> temp;
-        temp.push_back(l);
-        temp.push_back(r);
-        return temp;
-    }
-    
-    
-    vector<vector<int>> findMissingRanges(vector<int>& nums, int l, int r) {
-        vector<vector<int>> res;
-        int flag = 0;
-        int n = nums.size();
-        for(int i =0;i<nums.size();i++){
-            if(nums[i]>=l && nums[i]<=r){
-                flag = 1;
-                if(i==0){
-                    if(l<=nums[i]-1)
-                        res.push_back(func(l,nums[i]-1));
-                }else if(nums[i-1]<l){
-                    if(l<=nums[i]-1)
-                        res.push_back(func(l,nums[i]-1));
-                }
-                
-                if(i+1 == n){
-                     if(nums[i]+1<=r)
-                        res.push_back(func(nums[i]+1,r));
-                }else if (nums[i+1]>r){
-                    if(nums[i]+1<=r)
-                        res.push_back(func(nums[i]+1,r));
-                }else{
-                    if(nums[i]+1<=nums[i+1]-1)
-                        res.push_back(func(nums[i]+1,nums[i+1]-1));
-                }    
-            }
-
+    vector<vector<int>> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        vector<vector<int>> ans;
+        if(nums.size()==0){
+            ans.push_back({lower,upper});
+            return ans;
         }
-        
-        if(flag == 0){
-                res.push_back(func(l,r));
+        if(lower!=nums[0]){
+            ans.push_back({lower,nums[0]-1});
+        }
+          for(int i=0;i<nums.size()-1;i++){
+              if(nums[i]!=nums[i+1]-1){
+               ans.push_back({nums[i]+1,nums[i+1]-1});   
+              }
           }
-        return res;
+        if(upper!=nums[nums.size()-1]){
+            ans.push_back({nums[nums.size()-1]+1,upper});
+        }
+        return ans;
     }
 };
