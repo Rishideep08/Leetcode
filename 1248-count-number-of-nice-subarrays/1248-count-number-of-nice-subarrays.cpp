0b7a2e92d5890bddba138642ffac01ad0c6741f3
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-02-11 14:46:15 -0600

    Time: 85 ms (96.20%), Space: 70 MB (51.25%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -2,49 +2,31 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int start = 0;
-        int end =0;
-        int ans =0;
-        int possibleEnds= 0;
+        int end = 0;
+        int n = nums.size();
+        int ans = 0;
+        int substrWithEnd = 0;
         int count = 0;
-        int n= nums.size();
+
         while(end<n){
-            if(nums[end]%2 == 1){
-                count++;
+           if((nums[end]%2) != 0){
+               count++;
+           }
+           int possibleStarts = 0;
+           while(count==k){
+                if(nums[start]%2!=0){
+                    count--;
+                }   
+               start++;
+               possibleStarts++;
+           }
+            if((nums[end]%2)!=0){
+                substrWithEnd = possibleStarts;
             }
-            if(count == k){
-                // cout<<"Hi "<<start<<" "<<end<<endl;
-                possibleEnds++;
-                ans++;
-            }else if(count > k){
-                // cout<<"addfdf"<<endl;
-                if(nums[start]%2 == 1){
-                        count--;
-                }
-                start++;
-                while(count>k){
-                    ans = ans+possibleEnds;
-                    if(nums[start]%2 == 1){
-                        count--;
-                    }
-                    start++;
-                } 
-                possibleEnds = 1;
-                ans++;
-            }
+            ans = ans+substrWithEnd;
             end++;
         }
         
-        if(nums[start]%2 == 1){
-                count--;
-        }
-        start++;
-        while(start<n && count==k){
-            ans = ans+possibleEnds;
-            if(nums[start]%2 == 1){
-                count--;
-            }
-            start++;
-        } 
         return ans;
     }
 };
