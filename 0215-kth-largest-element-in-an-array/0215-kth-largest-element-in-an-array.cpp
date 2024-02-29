677be4adb3bbccf9dde400ad85f47509c4df4700
Rishideep08 <92270349+Rishideep08@users.noreply.github.com>
2024-02-29 13:41:42 -0600

    Time: 2231 ms (5.01%), Space: 57.7 MB (84.67%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        
-        int minVal = *min_element(nums.begin(),nums.end());
-        int maxVal = *max_element(nums.begin(),nums.end());
-        vector<int> temp(maxVal-minVal+1,0);
-        for(int i=0;i<nums.size();i++){
-            int val = nums[i];
-            temp[val-minVal]++;
-        }
-        int ans=0;
-        int i = temp.size()-1;
-        while(k>0){
-            if(temp[i]>=k){
-                return i+minVal;
-            }else{
-                k = k-temp[i];
-                i--;
+        return quickSelect(nums, k-1,0,nums.size()-1);
+    }
+    
+    int quickSelect(vector<int>& nums, int k,int l,int r) {
+        // cout<<"l : "<<l<<" r: "<<r<<endl;
+        int size = r-l+1;
+        int pivotIndex = (rand() % size)+l;
+        swap(nums[pivotIndex],nums[r]);
+        int pivot = nums[r];
+        // cout<<pivotIndex<<" "<<nums[r]<<endl;
+        int j = l-1;
+        int i = l;
+        while(l<r){
+            if(nums[l]>pivot){
+                j++;
+                swap(nums[l],nums[j]);
             }
+            l++;
         }
-        return ans+minVal;
+        j++;
+        swap(nums[j],nums[r]);
+//         for(int i=0;i<nums.size();i++){
+//             cout<<nums[i]<<" ";
+//         }
+//         cout<<endl;
         
+        if(j<k){
+            return quickSelect(nums,k,j+1,r);
+        }else if(j>k){
+            return quickSelect(nums,k,i,j-1);
+        }
+            
+        return nums[j];
     }
 };
